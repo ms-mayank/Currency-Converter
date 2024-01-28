9e4be2ff659ac04c7b2827bf46815f73288e3d42
Shukla <SHUKLM14@pfizer.com>
2024-01-28 18:15:52 +0530

    coverter

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+import Inputbox from "./Inputbox"
+
+export {Inputbox}
+
+/* This file is use to import all the components inside it and then import this file to particular place, so that if component are around large in no ie. 15/16 you won't have to individually import each component,
+Just import this file and all the components will be imported which are added in this file  */
